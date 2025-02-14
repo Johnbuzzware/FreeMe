40c522936798bfc731f63f6aeee41c821169bf89
Asad <Asadullah9555@gmail.com>
2025-02-14 22:40:04 +0500

    Initial Commit

---
+++ FreeMeTests/FreeMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FreeMeTests.swift
+//  FreeMeTests
+//
+//  Created by Asad's Mac on 14/02/2025.
+//
+
+import Testing
+@testable import FreeMe
+
+struct FreeMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
