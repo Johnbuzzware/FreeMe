630c509e1752645a15b5831e10d6986a816e9cda
Asad <johnbuzzware@gmail.com>
2025-05-08 00:25:30 +0500

    Profile

---
+++ FreeMe/Controller/AttorneyLsit/AttorneyListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AttorneyListViewController.swift
+//  FreeMe
+//
+//  Created by Asad's Mac on 07/05/2025.
+//
+
+import UIKit
+
+class AttorneyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
